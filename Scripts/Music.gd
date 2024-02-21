2e00f08b3f3e6629fc66565768440ca79d0c355e
Brian <88brianw@gmail.com>
2024-02-21 01:14:01 -0800

    NPC Dialogue + Lose Screen + Custom Tilemap

---
+++ Scripts/Music.gd
@@ -0,0 +1,10 @@
+extends Node
+
+func _on_normal_finished():
+	$Normal.play()
+	pass # Replace with function body.
+
+
+func _on_battle_finished():
+	$Battle.play()
+	pass # Replace with function body.
